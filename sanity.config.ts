57dd848ea41cf29f05cc96501a0285ba7ac8570a
Martin Hofbauer <m.hofbauer@email.cz>
2023-07-26 10:09:31 +0200

    change config file

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,14 +2,13 @@ import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './schemas'
-import { SANITY_PROJECT_ID, SANITY_DATASET } from '../src/config'
 
 export default defineConfig({
   name: 'default',
   title: 'band-website-sanity',
 
-  projectId: SANITY_PROJECT_ID,
-  dataset: SANITY_DATASET,
+  projectId: 'fk7ww56m',
+  dataset: 'production',
 
   plugins: [deskTool(), visionTool()],
 
